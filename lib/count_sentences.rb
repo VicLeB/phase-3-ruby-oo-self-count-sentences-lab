8ac0bf1f83cb29b5f275f7c3397202014d537dcb
Victoria LeBel <vickileb@hotmail.com>
2022-07-19 23:06:49 -0700

    commit tree 2115ee454861f4347c0d580163dc5f4fa9256947

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[!?.]/).filter do |x|
+      return !x.empty?
+    end.count
   end
 end
+
+const fn = () => x
+
+const fn2 = () => {
+  return
+}
